8a0cc2032da039900f8179de6cb2bcdbe58dfdd3
Promethicon <Promethicon@gmail.com>
2017-11-02 13:44:20 -0400

    Part 4 done

--- AngularFirstProject/src/app/app.component.ts
+++ AngularFirstProject/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
-export class Hero{
-  id: number;
-  name: string;
-}
 const HEROES: Hero [] = [
   {id: 1,name: 'Angly Joe'},
   {id: 2,name: 'Dr. Exploder'},
